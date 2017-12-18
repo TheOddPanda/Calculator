1dc7367fff56e417f249dd9e3c298a595b668ed9
5ia15adbuechi <5ia15adbuechi@gibwintra.ch>
2017-12-18 09:05:25 +0100

    Added Substraction

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,4 +6,8 @@ public final class Calculator {
     public static double addition(double summand1, double summand2) {
         return summand1 + summand2;
     }
+
+    public static double subtraction(double summand1, double summand2) {
+        return summand1 - summand2;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,24 +1,30 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 import static java.lang.Double.MIN_VALUE;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     @Test
     public void testAddition() {
         double addition = Calculator.addition(2.5, 7);
-        Assert.assertEquals(addition, 9.5, MIN_VALUE);
+        assertEquals(addition, 9.5, MIN_VALUE);
     }
 
     @Test
     public void testAdditionWithNegativeNumbers() {
         double addition = Calculator.addition(-2.5, -7);
-        Assert.assertEquals(addition, -9.5, MIN_VALUE);
+        assertEquals(addition, -9.5, MIN_VALUE);
     }
 
     @Test
     public void testAdditionWithZero() {
         double addition = Calculator.addition(0, 0);
-        Assert.assertEquals(addition, 0, MIN_VALUE);
+        assertEquals(addition, 0, MIN_VALUE);
+    }
+
+    @Test
+    public void testSubtraction() {
+        double subtraction = Calculator.subtraction(10, 5);
+        assertEquals(subtraction, 0, MIN_VALUE);
     }
 }
